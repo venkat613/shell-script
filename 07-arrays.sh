b831fc05ebfc7fecfa057a0a51410921bf851ba8
Venkat <kondalueao@gmail.com>
2023-12-12 20:11:14 +0530

    second

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bsh
+
+FRUITS=("apple" "mongo" "banana")
+
+echo "First fruit : ${FRUITS[0]}"
+
+echo "Second fruit : ${FRUITS[1]}"
+
+echo "Third fuit : ${FRUITS[2]}"
+
+echo "total :${FRUITS[@]}"
