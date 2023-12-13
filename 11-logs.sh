f69fe859cebaae8c90a5449f29b843e44252e687
Venkat <kondalueao@gmail.com>
2023-12-13 19:07:13 +0530

    first

---
+++ 11-logs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date +F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+if[ $ID -ne 0 ]
+then
+   echo "$R error please run as root user"
+else
+   echo "$G your root user"
+fi   
