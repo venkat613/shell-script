047b8f6fce810d1627bb42f8384ab1fcff11dadb
Venkat <kondalueao@gmail.com>
2023-12-14 19:12:48 +0530

    first

---
+++ 14-install-packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+    echo "error: please run as root user"
+else
+    echo "user root user"
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "instalation was failed"
+else
+    echo "instalation was success"
+fi
